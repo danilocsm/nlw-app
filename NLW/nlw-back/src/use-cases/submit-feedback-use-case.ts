b12118617ab8e05dbb208c591b7a453aca441c28
Danilo Medeiros <danilo.medeiros@splab.ufcg.edu.br>
2022-05-09 10:09:05 -0300

    addind front-end integration

--- NLW/nlw-back/src/use-cases/submit-feedback-use-case.ts
+++ NLW/nlw-back/src/use-cases/submit-feedback-use-case.ts
@@ -39,6 +39,7 @@ export class SubmitFeedbackUseCase {
         `<div style="fontfamily: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo de feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}/>` : null,
         `</div>`,
       ].join("\n"),
     });

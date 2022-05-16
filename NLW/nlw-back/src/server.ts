fa893a6532f39ea1287c9b7b36b57528c0bc5992
Danilo Medeiros <danilo.medeiros@splab.ufcg.edu.br>
2022-05-16 11:06:32 -0300

    added request size limit

--- NLW/nlw-back/src/server.ts
+++ NLW/nlw-back/src/server.ts
@@ -5,7 +5,7 @@ import { router } from "./routes";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(router);
 
 app.listen(process.env.PORT || 3333, () => {
